a11dd50aae878901dcc6342d9cb67af6a468ae77
M_H <mowz971@gmail.com>
2014-02-06 21:39:43 +0000

    sendEmail func built but still bugs present

--- js/main.js
+++ js/main.js
@@ -214,9 +214,11 @@ function sign() {
 			 
 			user.signUp(null, {
 			  success: function(user) {
-			    // Hooray! Let them use the app now.
+			    
 			    alert("You have successfully signed up now! Go back and login with your new Username and Password!");
-			    email();
+			    
+			    sendEmail(newName, email);
+			    
 			  },
 			  error: function(user, error) {
 			    // Show the error message somewhere and let the user try again.
--- js/mandrillSub.js
+++ js/mandrillSub.js
@@ -1,48 +1,54 @@
-function email() {
+function sendEmail(name, email) {
+    var name = name;
+    var email = email;
+    
 	console.log("New Email recieved:" + email);
-	console.log("New Username recieved: " + newName);
-};
+	console.log("New Username recieved: " + name);
 
-// Create a function to log the response from the Mandrill API
-function log(obj) {
-    $('#response').text(JSON.stringify(obj));
-}
+    /*// Create a function to log the response from the Mandrill API
+    function log(obj) {
+        //$('#response').text(JSON.stringify(obj));
+        alert(JSON.stringify(obj));
+        console.log(JSON.stringify(obj));
+    }*/
 
-// create a new instance of the Mandrill class with your API key
-var m = new mandrill.Mandrill('tQbyx1-idFgbo1qVCBij-Q');
+    // create a new instance of the Mandrill class with your API key
+    var m = new mandrill.Mandrill('tQbyx1-idFgbo1qVCBij-Q');
 
-// create a variable for the API call parameters
-var params = {
-    "message": {
-        "from_email":"mowz971@gmail.com",
-        "to":[{"email": email}],
-        "subject": "Welcome to the Fluent Web App *|dude|*!",
-        "html": "<p>Dear *|dude|*,</p><p>Please click the link below to confirm your subscription to the Fluent web app</p><img src=\"http://www.bjmlegal.co.uk/wp-content/uploads/2013/09/hello-1774.jpg\">",
-        "autotext": true,
-        "track_opens" : true,
-        "track_clicks" : true,
-        
-        "merge_vars": [
-            {
-                "rcpt": "mowz971@gmail.com",
-                "vars": [
-                    {
-                        "name": "dude",
-                        "content": newName
-                    },
-                ]
-            }
-        ]
-        
+    // create a variable for the API call parameters
+    var params = {
+        "message": {
+            "from_email":"mowz971@gmail.com",
+            "to":[{"email": email}],
+            "subject": "Welcome to the Fluent Web App *|dude|*!",
+            "html": "<p>Dear *|dude|*,</p><p>Please click the link below to confirm your subscription to the Fluent web app</p><img src=\"http://www.bjmlegal.co.uk/wp-content/uploads/2013/09/hello-1774.jpg\">",
+            "autotext": true,
+            "track_opens" : true,
+            "track_clicks" : true,
+            
+            "merge_vars": [
+                {
+                    "rcpt": "mowz971@gmail.com",
+                    "vars": [
+                        {
+                            "name": "dude",
+                            "content": name
+                        },
+                    ]
+                }
+            ]
+            
+        }
+    };
+
+    function sendTheMail() {
+    // Send the email!
+
+        m.messages.send(params, function(res) {
+            log(res);
+        }, function(err) {
+            log(err);
+        });
     }
+
 };
-
-function sendTheMail() {
-// Send the email!
-
-    m.messages.send(params, function(res) {
-        log(res);
-    }, function(err) {
-        log(err);
-    });
-}
